0536f2bb63eb10a8b92197846baf41414f7cc328
stomaras <sptoma724@gmail.com>
2021-02-05 15:48:10 +0200

    Composition part 2 PC

--- Composition/src/com/company/Main.java
+++ Composition/src/com/company/Main.java
@@ -10,10 +10,9 @@ public class Main {
         Monitor theMonitor = new Monitor("27inch Beast", "Acer", 27, new Resolution(2540,1440));
 
         Motherboard theMotherboard = new Motherboard("BJ-200", "Asus", 4,6,"v2.44");
-
+        // Composition is actually creating object within object. So you have a master object in this case thePC
+        // is managing and looking after, and this is using composition to achieve that
         PC thePC = new PC(theCase, theMonitor, theMotherboard);
-        thePC.getMonitor().drawPixelAt(1500, 1200, "red");
-        thePC.getMotherboard().loadProgram("Linux");
-        thePC.getTheCase().pressPowerButton();
+       thePC.powerUp();
     }
 }
--- Composition/src/com/company/PC.java
+++ Composition/src/com/company/PC.java
@@ -11,16 +11,29 @@ public class PC {
         this.motherboard = motherboard;
     }
 
-    public Case getTheCase() {
+    public void powerUp() {
+        theCase.pressPowerButton();
+        drawLogo();
+    }
+
+    private void drawLogo() {
+        // Fancy Graphics
+        monitor.drawPixelAt(1200, 50, "yellow");
+    }
+
+    /*
+    private Case getTheCase() {
         return theCase;
     }
 
-    public Monitor getMonitor() {
+    private Monitor getMonitor() {
         return monitor;
     }
 
-    public Motherboard getMotherboard() {
+    private Motherboard getMotherboard() {
         return motherboard;
     }
+    */
+
 
 }
