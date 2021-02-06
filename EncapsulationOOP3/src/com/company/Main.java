f00819a9ed9a08f0c2b5913adeb8055c540106af
stomaras <sptoma724@gmail.com>
2021-02-06 18:27:07 +0200

    EncapsulationOOP3

---
+++ EncapsulationOOP3/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        EnhancedPlayer player = new EnhancedPlayer("Tim", 50, "Sword");
+        System.out.println("Initial health is " + player.getHealth());
+    }
+}
