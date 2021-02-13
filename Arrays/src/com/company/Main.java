01ecef28144431d8245cbfd5d31c65c47f21414b
stomaras <sptoma724@gmail.com>
2021-02-13 14:59:07 +0200

    Arrays

---
+++ Arrays/src/com/company/Main.java
@@ -0,0 +1,40 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+    // System.in allows you to type input into the console that gets returned back to the program.
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+	// write your code here
+        int[] myIntegers = getIntegers(5);
+        for (int i=0; i<myIntegers.length; i++){
+            System.out.println("Element " + i + ", typed value was " + myIntegers[i]);
+        }
+        System.out.println("The average is " + getAverage(myIntegers));
+
+    }
+
+    public static int[] getIntegers(int number) {
+        System.out.println("Enter " + number + " integer values.\r");
+        int[] values = new int[number];
+
+        for (int i=0; i<values.length; i++) {
+            // the scanner.nectInt automatically accepts an integer from the console.
+            values[i] = scanner.nextInt();
+        }
+
+        return values;
+    }
+
+    public static double getAverage(int[] array) {
+        int sum = 0;
+        for (int i=0; i<array.length; i++) {
+            sum+= array[i];
+        }
+        return (double) sum / (double)array.length;
+
+    }
+
+}
