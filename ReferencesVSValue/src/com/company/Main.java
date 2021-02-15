0fe353972907dbd331bd8abf852d3f89ade95b45
stomaras <sptoma724@gmail.com>
2021-02-15 16:47:41 +0200

    Cool

---
+++ ReferencesVSValue/src/com/company/Main.java
@@ -0,0 +1,45 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int myIntValue = 10;
+        int anotherIntValue = myIntValue;
+
+        System.out.println("myIntValue = " + myIntValue);
+        System.out.println("anotherIntValue = " + anotherIntValue);
+
+        anotherIntValue++;
+
+        System.out.println("myIntValue = " + myIntValue);
+        System.out.println("anotherIntValue = " + anotherIntValue);
+
+        // we have two references pointing to the same array in memory
+        int[] myIntArray = new int[5];
+        int[] anotherArray = myIntArray;
+
+        // toString method convert every elemnt into array to String .
+        System.out.println("myIntArray= " + Arrays.toString(myIntArray));
+        System.out.println("anotherArray = " + Arrays.toString((anotherArray)));
+
+        anotherArray[0] = 1;
+
+        System.out.println("after change myIntArray= " + Arrays.toString(myIntArray));
+        System.out.println("after change anotherArray= " + Arrays.toString(anotherArray));
+
+        modifyArray(myIntArray);
+
+        System.out.println("after modify myIntArray= " + Arrays.toString(myIntArray));
+        System.out.println("after modify anotherArray= " + Arrays.toString(anotherArray));
+
+    }
+
+    private static void modifyArray(int[] array) {
+
+        array[0] = 2;
+        array = new int[] {1, 2, 3, 4, 5};
+    }
+}
