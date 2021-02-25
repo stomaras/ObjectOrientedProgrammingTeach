11b978cb9d0e3c5d44ec779daa63d6aac7f5e00c
stomaras <sptoma724@gmail.com>
2021-02-25 11:30:18 +0200

    Banking Application 50% done

--- BankingApplication/src/com/company/Bank.java
+++ BankingApplication/src/com/company/Bank.java
@@ -1,5 +1,49 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Bank {
 
+    private String name;
+    private ArrayList<Branch> branches;
+
+    public Bank(String bankName) {
+        this.name = bankName;
+        this.branches = new ArrayList<Branch>();
+    }
+
+    private Branch findBranch(String branchName) {
+        for (int i = 0; i< this.branches.size(); i++) {
+            Branch branch = this.branches.get(i);
+            if (branch.getName().equals(branchName)) {
+                return branch;
+            }
+        }
+        return null;
+    }
+
+    public boolean addBranch(String branchName){
+        if (findBranch(branchName) == null){
+            Branch newBranch = new Branch(branchName);
+            this.branches.add(newBranch);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean addCustomer(String branchName, String customerName, double initialTransaction) {
+        if (findBranch(branchName) != null){
+            Branch branch = new Branch(branchName);
+            return branch.newCustomer(branchName,initialTransaction);
+        }
+        return false;
+    }
+
+    public boolean addCustomerTransaction(String branchName, String customerName, double initialTransaction) {
+        Branch branch = findBranch(branchName);
+        if (branch != null) {
+            return branch.addCustomerTransaction(branchName, initialTransaction);
+        }
+        return false;
+    }
 }
