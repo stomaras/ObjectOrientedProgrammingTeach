c5b3e434771576bc1a5a121cfe4ec651e1ad77ac
stomaras <sptoma724@gmail.com>
2021-03-01 18:50:47 +0200

    Bank class start

--- Bank/src/com/company/Bank.java
+++ Bank/src/com/company/Bank.java
@@ -4,5 +4,26 @@ import java.util.ArrayList;
 
 public class Bank {
     // it has two fields. A String called name and an ArrayList that holds objects of type Branch called branches
+    private String name;
+    private ArrayList<Branch> branches;
 
+    // A constructor that takes a String(name of the bank). It initialises name and instantiates branches
+
+    public Bank(String name) {
+        this.name = name;
+        this.branches = new ArrayList<Branch>();
+    }
+
+    // findBranch(), has one parameter of type String (name of the Branch) and returns a Branch. Returns the
+    // Branch if it exists or null otherwise.
+
+    private Branch findBranch(String nameBranch) {
+        for (int i = 0; i < this.branches.size(); i++) {
+            if (this.branches.get(i).getName() == nameBranch) {
+                Branch branch = new Branch(nameBranch);
+                return branch;
+            }
+        }
+        return null;
+    }
 }
