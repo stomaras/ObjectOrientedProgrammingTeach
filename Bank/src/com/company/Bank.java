ca7f31229efb2702fa2a890155f6504166cac70d
stomaras <sptoma724@gmail.com>
2021-03-01 17:21:48 +0200

    Customer class done

---
+++ Bank/src/com/company/Bank.java
@@ -0,0 +1,8 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Bank {
+    // it has two fields. A String called name and an ArrayList that holds objects of type Branch called branches
+
+}
---
+++ Bank/src/com/company/Customer.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Customer {
+    // it has two fields , A String called name and an ArrayList that holds objects of type Double called transactions
+    private String name;
+    private ArrayList<Double> transactions;
+
+    // A constructor that takes a String (name of customer) and a double (initial transaction).It initialises name and
+    // instantiates transactions.
+
+
+    public Customer(String name) {
+        this.name = name;
+        this.transactions = new ArrayList<Double>();
+    }
+
+    // And three methods, they are (their functions are in their names):
+    // getName(), getter for name
+    // getTransactions(), getter for transactions
+    // addTransaction(), has one parameter of type double(transaction) and does not return anything
+
+    public String getName() {
+        return this.name;
+    }
+
+    public ArrayList<Double> getTransactions() {
+        return this.transactions;
+    }
+
+    public void addTransaction(double transaction) {
+        this.transactions.add(transaction);
+    }
+
+}
