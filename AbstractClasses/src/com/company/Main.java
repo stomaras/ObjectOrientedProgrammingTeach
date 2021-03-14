174f7c264ae579348cd904eebfb3cd60b6dd55a7
stomaras <sptoma724@gmail.com>
2021-03-14 10:21:46 +0200

    Theory about interfaces and abstract classes

---
+++ AbstractClasses/src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        // Abstraction is when you define the required functionality for something without actually
+        // implementing the data house.
+        // As i mention , interfaces are by definition, in Java, Abstract.
+        // Abstract class vs Interface
+        // 1. Type of methods : Interface can have only abstract methods. Abstract class can have abstarct and
+              // non-abstract methods.
+        // 2. Type of variables : Abstract class can have final, non-final, static and non-static variables.Interfaces
+              // has only static and final variables.
+        // 3. Type of variables: Abstract class can have final, non-final, static and non-static variables. Interface
+              // has only static and final variables.
+        // 4. Implementation: Abstract class can provide the implementation of interface. Interface can not provide
+             // the implementation of abstract class.
+        // 5. Inheritance vs Abstraction : A Java interface can be implemented using keyword " implements " and
+             // abstract class can be extended using keyword "extends"
+        // 6. Multiple implementation: An interface can extend another java interface only, an abstract class can
+            // extend another java class and implement multiple Java interfaces.
+        // 7. Accesibility of Data Members: Members of a java interface are public by default. A java abstract class can have
+           // class members like private, protected, e.t.c, e.t.c.
+
+    }
+}
