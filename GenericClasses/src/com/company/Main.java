29829d6965dac36e2a3078ae0e0f753e8c6aaa6e
stomaras <sptoma724@gmail.com>
2021-03-15 18:22:50 +0200

    Generic Classes

---
+++ GenericClasses/src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        FootballPlayer joe = new FootballPlayer("Joe");
+        BaseballPlayer pat = new BaseballPlayer("Pat");
+        SoccerPlayer beckam = new SoccerPlayer("Beckam");
+
+        Team adelaideCrows = new Team("Adelaide Crows");
+        adelaideCrows.addPlayer(joe);
+        adelaideCrows.addPlayer(pat);
+        adelaideCrows.addPlayer(beckam);
+
+        System.out.println(adelaideCrows.numPlayers());
+    }
+}
---
+++ GenericClasses/src/com/company/Team.java
@@ -0,0 +1,61 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Team {
+
+    private String name;
+    int played = 0;
+    int won = 0;
+    int lost = 0;
+    int tied = 0;
+
+    // Player is an abstract class that all the other three classes extend from.
+
+    private ArrayList<Player> members = new ArrayList<>();
+
+    public Team(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean addPlayer(Player player) {
+        if (members.contains(player)) {
+            System.out.println(player.getName() + " is already on this team ");
+            return false;
+        } else {
+            members.add(player);
+            System.out.println(player.getName() + " picked for team " + this.name);
+            return true;
+        }
+    }
+
+    public int numPlayers() {
+        return this.members.size();
+    }
+
+    public void matchResult(Team opponent, int ourScore, int theirScore) {
+        if (ourScore > theirScore) {
+            won++;
+        } else if (ourScore == theirScore) {
+            tied++;
+        } else {
+            lost++;
+        }
+        played++;
+        // with this way we can actually also update the opponent's score.
+        // we are also saving the results in the opponent's team as well.
+        if (opponent != null) {
+            opponent.matchResult(null, theirScore, ourScore);
+        }
+    }
+
+    public int ranking() {
+        return (won * 2) + tied;
+    }
+
+
+}
