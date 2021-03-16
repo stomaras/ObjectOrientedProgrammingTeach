d6530bf2db741f688ed5c3bd36083ec866a67ac3
stomaras <sptoma724@gmail.com>
2021-03-16 19:25:50 +0200

    Generic Classes Part 2

--- GenericClasses/src/com/company/Main.java
+++ GenericClasses/src/com/company/Main.java
@@ -8,11 +8,25 @@ public class Main {
         BaseballPlayer pat = new BaseballPlayer("Pat");
         SoccerPlayer beckam = new SoccerPlayer("Beckam");
 
-        Team adelaideCrows = new Team("Adelaide Crows");
+        // This particular instance is gonna acceept only football players
+        Team<FootballPlayer> adelaideCrows = new Team<>("Adelaide Crows");
         adelaideCrows.addPlayer(joe);
-        adelaideCrows.addPlayer(pat);
-        adelaideCrows.addPlayer(beckam);
+        Team<BaseballPlayer> baseballTeam = new Team<>("Chicago Cubs");
+        baseballTeam.addPlayer(pat);
+        Team<SoccerPlayer> soccerTeam = new Team<>("Los Angeles Lakers");
+        soccerTeam.addPlayer(beckam);
+
+        Team<FootballPlayer> hawthorn = new Team<>("Hawthorn");
+        Team<FootballPlayer> fremantle = new Team<>("Fremantle");
+
+        hawthorn.matchResult(fremantle, 1, 0);
+        hawthorn.matchResult(adelaideCrows, 3, 8);
 
         System.out.println(adelaideCrows.numPlayers());
+        // We need some sort of mechanism in order to confirm that only the right types of players can be added to teams
+        // One solution could be to create 3 different team classes, so in other words, a football team, a baseball team and
+        // a soccer team are three different classes and ensure that they only accept the correct player type. So in this case this will be
+        // duplicating code and it's usually never an efficient way to code to actually duplicate codes so i do not recommend that.
+        //
     }
 }
--- GenericClasses/src/com/company/Team.java
+++ GenericClasses/src/com/company/Team.java
@@ -2,7 +2,7 @@ package com.company;
 
 import java.util.ArrayList;
 
-public class Team {
+public class Team<T> {
 
     private String name;
     int played = 0;
@@ -12,7 +12,7 @@ public class Team {
 
     // Player is an abstract class that all the other three classes extend from.
 
-    private ArrayList<Player> members = new ArrayList<>();
+    private ArrayList<T> members = new ArrayList<>();
 
     public Team(String name) {
         this.name = name;
@@ -22,13 +22,13 @@ public class Team {
         return name;
     }
 
-    public boolean addPlayer(Player player) {
+    public boolean addPlayer(T player) {
         if (members.contains(player)) {
-            System.out.println(player.getName() + " is already on this team ");
+            System.out.println(((Player)player).getName() + " is already on this team ");
             return false;
         } else {
             members.add(player);
-            System.out.println(player.getName() + " picked for team " + this.name);
+            System.out.println(((Player)player).getName() + " picked for team " + this.name);
             return true;
         }
     }
@@ -37,18 +37,25 @@ public class Team {
         return this.members.size();
     }
 
-    public void matchResult(Team opponent, int ourScore, int theirScore) {
+    public void matchResult(Team<T> opponent, int ourScore, int theirScore) {
+
+        String message;
+
         if (ourScore > theirScore) {
             won++;
+            message = " beat ";
         } else if (ourScore == theirScore) {
             tied++;
+            message = " drew with ";
         } else {
             lost++;
+            message = " lost to ";
         }
         played++;
         // with this way we can actually also update the opponent's score.
         // we are also saving the results in the opponent's team as well.
         if (opponent != null) {
+            System.out.println(this.getName() + message + opponent.getName());
             opponent.matchResult(null, theirScore, ourScore);
         }
     }
